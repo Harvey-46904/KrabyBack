d5e5bf2660f42f380c569bce87463e2bb7530975
Yidwar <mikeyidwar@gmail.com>
2023-04-25 09:07:58 -0500

    cambios mike

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('comercial','CentroComercialesController',['except'=>['create','edit']]);
+Route::resource('menu','MenuController',['except'=>['create','edit']]);
+Route::resource('pedido','PedidosController',['except'=>['create','edit']]);
+Route::resource('restaurante','RestaurantesController',['except'=>['create','edit']]);
+Route::resource('venta','VentasController',['except'=>['create','edit']]);
